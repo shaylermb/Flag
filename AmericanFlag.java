a4b62102c49a4f557c4dff2e8f2afdcde07cfe20
apcs <apcs@mvhs-u528ap04.dcsdk12.org>
2013-10-22 09:06:48 -0600

    classwork

---
+++ AmericanFlag.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+public class AmericanFlag {
+	private int length;
+	private int height;
+	private double scale;
+	public AmericanFlag(int length, int height, double scale){
+		this.height = height;
+		this.length = length;
+		this.scale = scale;
+	}
+	
+}
---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+import java.awt.Color;
+public class Rectangle {
+	private int length;
+	private int height;
+	private Color color;
+	public Rectangle(int length, int height, Color color){
+		this.length = length;
+		this.height = height/13;
+		this.color = color;
+	}
+	public Union(){
+
+	}
+
+}
